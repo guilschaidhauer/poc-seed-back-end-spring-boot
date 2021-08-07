2d022eeb468a4157086715b16c61af6baca017ed
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2021-08-07 14:18:05 -0300

    Add post, delete and patch to book controller

--- src/main/java/com/gsc/pocseedspringboot/controllers/BooksController.java
+++ src/main/java/com/gsc/pocseedspringboot/controllers/BooksController.java
@@ -2,11 +2,9 @@ package com.gsc.pocseedspringboot.controllers;
 
 import com.gsc.pocseedspringboot.models.Book;
 import com.gsc.pocseedspringboot.repositories.BookRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,23 @@ public class BooksController {
     @RequestMapping("{id}")
     public Book get(@PathVariable Long id) {
         return bookRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Book create(@RequestBody final Book book) {
+        return bookRepository.saveAndFlush(book);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PATCH)
+    public Book update(@PathVariable Long id, @RequestBody Book book) {
+        //TODO: Add validation
+        Book existingBook = bookRepository.getOne(id);
+        BeanUtils.copyProperties(book, existingBook, "id");
+        return bookRepository.saveAndFlush(existingBook);
     }
 }
--- src/main/java/com/gsc/pocseedspringboot/models/Book.java
+++ src/main/java/com/gsc/pocseedspringboot/models/Book.java
@@ -1,11 +1,14 @@
 package com.gsc.pocseedspringboot.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name="books")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
