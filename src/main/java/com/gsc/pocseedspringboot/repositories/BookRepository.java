88623ee26d2a85b073fb01f08ee47919a35b9115
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2021-08-07 13:35:27 -0300

    Add book repository

---
+++ src/main/java/com/gsc/pocseedspringboot/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package com.gsc.pocseedspringboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.awt.print.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
