b26519d778c6fb72a9d6decd9e8b3f0df8e21a07
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2021-08-07 13:29:18 -0300

    Add book model getters and setters

--- src/main/java/com/gsc/pocseedspringboot/model/Book.java
+++ src/main/java/com/gsc/pocseedspringboot/model/Book.java
@@ -18,4 +18,35 @@ public class Book {
 
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 }
