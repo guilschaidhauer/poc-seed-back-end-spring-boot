278e3abd2560d0361fa66adaf0f3a2ebefd4fe34
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2021-08-07 12:10:32 -0300

    Add base project

---
+++ src/test/java/com/gsc/pocseedspringboot/PocSeedSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsc.pocseedspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSeedSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
